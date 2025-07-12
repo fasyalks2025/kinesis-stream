8bcfb5602daaa308bbd5b3d2b05e1a22eabc063d
Kamal Fasya <kamalfasya01@gmail.com>
2025-07-12 15:05:45 +0700

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# kinesis-stream
+
+
---
+++ main.py
@@ -0,0 +1,33 @@
+import boto3
+import json
+import time
+import random
+
+stream_name = 'SensorDataStream' # NOTE: change according to your kinesis stream name
+region_name = 'ap-southeast-1' # NOTE: change according to your region
+
+kinesis_client = boto3.client('kinesis', region_name=region_name)
+
+def generate_random_data():
+    return {
+        "sensor_id": random.randint(1, 10),
+        "temperature": round(random.uniform(20.0, 30.0), 2),
+        "humidity": round(random.uniform(30.0, 60.0), 2),
+        "timestamp": time.time()
+    }
+
+def send_data_to_kinesis():
+    while True:
+        data = generate_random_data()
+        partition_key = str(data['sensor_id'])
+        print(f"Sending data: {data}")
+        kinesis_client.put_record(
+            StreamName=stream_name,
+            Data=json.dumps(data),
+            PartitionKey=partition_key
+        )
+        time.sleep(1)
+
+if __name__ == "__main__":
+    print("Streaming random sensor data to Kinesis...")
+    send_data_to_kinesis()
